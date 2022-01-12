aa7720e545888fe76ea5b721dafbc0160d9420af
Mark Tenenholtz <marktenenholtz@gmail.com>
2022-01-11 21:32:18 -0500

    change pseudo ckpt name

--- pseudo_run.sh
+++ pseudo_run.sh
@@ -1,4 +1,5 @@
-python pseudo_train.py --model_name swin_large_patch4_window7_224_in22k --name random_resize --lr 2e-5 --wd 1e-2 --batch_size 8 --accumulate_grad_batches 4 --seed 26
-python pseudo_train.py --model_name swin_large_patch4_window7_224_in22k --name random_resize --lr 2e-5 --wd 1e-2 --batch_size 8 --accumulate_grad_batches 4 --seed 42
+python pseudo_train.py --model_name swin_large_patch4_window7_224_in22k --name random_resize_reg_aug --lr 2e-5 --wd 1e-2 --batch_size 8 --accumulate_grad_batches 4
+python pseudo_train.py --model_name swin_large_patch4_window7_224_in22k --name random_resize_reg_aug --lr 2e-5 --wd 1e-2 --batch_size 8 --accumulate_grad_batches 4 --seed 26
+python pseudo_train.py --model_name swin_large_patch4_window7_224_in22k --name random_resize_reg_aug --lr 2e-5 --wd 1e-2 --batch_size 8 --accumulate_grad_batches 4 --seed 42
 # python pseudo_train.py --model_name swin_base_patch4_window12_384_in22k --name random_resize --lr 2e-5 --wd 1e-2 --batch_size 4 --accumulate_grad_batches 8 --img_size_x 384 --img_size_y 384
 # python pseudo_train.py --model_name eca_nfnet_l2 --name random_crop --lr 2e-5 --wd 0. --batch_size 3 --accumulate_grad_batches 9 --img_size_x 512 --img_size_y 512
